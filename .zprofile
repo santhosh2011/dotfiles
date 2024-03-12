a9689667250c2b25a6df455b6dc5a5094a5a2ed6
Santhosh Thota <santhoshthota@Santhoshs-MacBook-Pro-2.local>
2024-03-12 15:21:16 +0530

    Adding zprofile and nvim

---
+++ .zprofile
@@ -0,0 +1,3 @@
+alias lg="lazygit"
+alias dotfiles="cd ~/code/dotfiles/"
+eval "$(/opt/homebrew/bin/brew shellenv)"
--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
 brew install neovim
+brew install koekeishiya/formulae/yabai
+yabai --start-service
+brew install koekeishiya/formulae/skhd
+skhd --start-service
+
 brew install stow
 
 stow .
